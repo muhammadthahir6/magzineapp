cea27dc4692a70693cfcce02d6485ba72ef4eeb4
muhammad <muhammadthahir6@gmail.com>
2018-09-03 20:56:28 +0530

    Done Almost

--- app/src/main/java/com/sctce/armagazine/ExtrasActivity.java
+++ app/src/main/java/com/sctce/armagazine/ExtrasActivity.java
@@ -1,13 +1,37 @@
 package com.sctce.armagazine;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Toast;
 
 public class ExtrasActivity extends AppCompatActivity {
-
+    CardView ToStore,MagSite;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extras);
+        ToStore=findViewById(R.id.playstoreAr);
+        ToStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Also Ask Your Friends for the Module(apk Installation)",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.facebook.katana"));
+                startActivity(intent);
+            }
+        });
+        MagSite=findViewById(R.id.magsite);
+        MagSite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://sctcollegemagazine.in/");
+                Intent intent= new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
     }
 }
